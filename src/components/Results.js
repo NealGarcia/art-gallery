cda0b7e12373e842ba8a7adf6e8f54780fe3522b
nealgarcia <nathnlgarcia@gmail.com>
2021-11-29 15:12:25 -0600

    added useEffect and fetch request

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,7 +1,17 @@
 import React, { useState, useEffect} from 'react';
 
 function Results() {
-    const [result, showResult] = useState([]);
+    const [result, setResult] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.artic.edu/api/v1/artworks')
+        .then(res => res.json())
+        .then(json => {
+            setResult(json)
+            console.log(json)
+        })
+        .catch(console.err)
+    },[])
 
     return (
         <div>
