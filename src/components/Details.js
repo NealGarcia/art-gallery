4eeb62543f916abde6b6c25849c261ce65f34521
nealgarcia <nathnlgarcia@gmail.com>
2021-12-04 22:09:18 -0600

    added more elements such as style and origin to details component

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,41 +1,37 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
 function Details(props) {
-    const [data, setData] = useState([])
-    const url = `https://api.artic.edu/api/v1/artworks/${props.match.params.search}`
-    console.log(url)
+  const [data, setData] = useState([]);
+  const url = `https://api.artic.edu/api/v1/artworks/${props.match.params.search}`;
+  console.log(url);
 
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch(console.err);
+  }, []);
 
-    useEffect(() => {
-        fetch(url)
-        .then(response => response.json())
-        .then(response => { 
-            setData(response.data)
-        })
-        .catch(console.err)
-    }, []);
-
-    return (
-        <div className = "detailsContainer">
-            {console.log(data)}
-            <img 
-                src = {`https://www.artic.edu/iiif/2/${data.image_id}/full/843,/0/default.jpg`} 
-                alt = {data.title}
-                className = "descriptionImage">
-            </img>
-            <div className = "descriptionText">
-                <h3 className = "artistName">{(data.artist_display)}</h3>
-                <h3 className = "artworkTitle">{data.title}</h3>
-                <h4 className = "artworkMedium">{data.medium_display}</h4>
-                <a href = "#">Read More</a>
-                <p>Go Back</p>
-
-
-
-
-            </div>
-        </div>
-    );
+  return (
+    <div className="detailsContainer">
+      {console.log(data)}
+      <img
+        src={`https://www.artic.edu/iiif/2/${data.image_id}/full/843,/0/default.jpg`}
+        alt={data.title}
+        className="descriptionImage"
+      ></img>
+      <div className="descriptionText">
+        <h3 className="artworkTitle">{data.title}</h3>
+        <h3 className="artistName">{data.artist_display}</h3> 
+        <h4 className="artworkDetails">Created in {data.place_of_origin}, {data.date_display}</h4>
+        <p className="artworkMedium"><b>Medium:</b> {data.medium_display}</p>
+        <p className="artowrkStyle"><b>Style:</b> {data.style_title}</p>
+        
+      </div>
+    </div>
+  );
 }
 
 export default Details;
